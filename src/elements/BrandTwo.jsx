d25b1f3a03cb7fe88f4c3f07032a9113f3ee7056
Jack <jackacbuchanan@gmail.com>
2022-07-15 00:12:09 +0100

    Added my project

---
+++ src/elements/BrandTwo.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+class BrandTwo extends Component{
+    render(){
+        return(
+            <React.Fragment>
+                <ul className="brand-style-2">
+                    <li>
+                        <img src="/assets/images/brand/expedia.png" alt="Logo Images"/>
+                    </li>
+                    <li>
+                        <img src="/assets/images/brand/tr.png" alt="Logo Images"/>
+                    </li>
+                    <li>
+                        <img src="/assets/images/brand/SpliceyImage.png" alt="Logo Images"/>
+                    </li>
+                    <li>
+                        <img src="/assets/images/brand/hotels.png" alt="Logo Images"/>
+                    </li>
+                    <li>
+                        <img src="/assets/images/brand/trivago.jpg" alt="Logo Images"/>
+                    </li>
+                    <li>
+                        <img src="/assets/images/brand/sure-cloud.png" alt="Logo Images"/>
+                    </li>
+                    <li>
+                        <img src="/assets/images/brand/flrlogo.png" alt="Logo Images"/>
+                    </li>
+                    <li>
+                        <img src="/assets/images/brand/vision-direct.png" alt="Logo Images"/>
+                    </li>
+                </ul>
+            </React.Fragment>
+        )
+    }
+}
+export default BrandTwo;
